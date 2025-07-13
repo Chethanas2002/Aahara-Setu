daf2635055059d4f8dc57d42b0fe9bb2830f7044
Chethana <chethanas1302@gmail.com>
2025-07-13 20:02:20 +0530

    feat: add one-to-one relationship between User and UserDetails entities

--- aaharaSetu/src/main/java/com/spring/aaharaSetu/model/User.java
+++ aaharaSetu/src/main/java/com/spring/aaharaSetu/model/User.java
@@ -1,7 +1,9 @@
 package com.spring.aaharaSetu.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +18,8 @@ public class User {
 	@Id
 	private long userId;
 	private String userName;
-
+	
+	@OneToOne(mappedBy="user" , cascade = CascadeType.ALL)
+	private UserDetails userDeatils;
 
 }
--- aaharaSetu/src/main/java/com/spring/aaharaSetu/model/UserDetails.java
+++ aaharaSetu/src/main/java/com/spring/aaharaSetu/model/UserDetails.java
@@ -1,7 +1,10 @@
 package com.spring.aaharaSetu.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,12 +16,15 @@ import lombok.NoArgsConstructor;
 @Entity
 public class UserDetails {
 	
-	
 	@Id
 	private long userDetailsId;
 	private String address;
 	private String email;
 	private long phone;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "userId")
+	private User user;
 	
 
 }
