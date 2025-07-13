205b485202aacaa90db339a68aaf4840a559474b
Chethana <chethanas1302@gmail.com>
2025-07-13 20:57:05 +0530

    feat: add one-to-many relationship between State and City entities

--- aaharaSetu/src/main/java/com/spring/aaharaSetu/model/City.java
+++ aaharaSetu/src/main/java/com/spring/aaharaSetu/model/City.java
@@ -1,7 +1,10 @@
 package com.spring.aaharaSetu.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,5 +20,9 @@ public class City {
 	private long cityId;
 	private String cityName;
 
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="stateId")
+	private State state;
 
 }
--- aaharaSetu/src/main/java/com/spring/aaharaSetu/model/State.java
+++ aaharaSetu/src/main/java/com/spring/aaharaSetu/model/State.java
@@ -1,7 +1,11 @@
 package com.spring.aaharaSetu.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,5 +20,9 @@ public class State {
 	@Id
 	private long stateId;
 	private String stateName;
+	
+	@OneToMany(mappedBy = "state" , cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
+	private List<City> city;
+	
 
 }
