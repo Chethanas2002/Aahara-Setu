3182af2303b1692324435eb9f4ab48bd6bfa90fe
Chethana <chethanas1302@gmail.com>
2025-07-13 08:45:11 +0530

    Initial Spring Boot setup with dependencies including Spring Web Spring Data JPA MySQL Driver Lombok Spring Boot DevTools

---
+++ aaharaSetu/src/main/java/com/spring/aaharaSetu/AaharaSetuApplication.java
@@ -0,0 +1,13 @@
+package com.spring.aaharaSetu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaharaSetuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaharaSetuApplication.class, args);
+	}
+
+}
---
+++ aaharaSetu/src/test/java/com/spring/aaharaSetu/AaharaSetuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.aaharaSetu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaharaSetuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
