5730b1691fc1e68bfb9135d4df08c09e15aca13b
Chethana <chethanas1302@gmail.com>
2025-07-13 22:48:39 +0530

    feat: add many-to-many mapping between User and Reel

--- aaharaSetu/src/main/java/com/spring/aaharaSetu/model/Reel.java
+++ aaharaSetu/src/main/java/com/spring/aaharaSetu/model/Reel.java
@@ -1,9 +1,13 @@
 package com.spring.aaharaSetu.model;
 
+import java.util.List;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,5 +27,13 @@ public class Reel {
 	@ManyToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "hotelId")
 	private Hotel hotel;
+	
+	
+	
+	@ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
+	@JoinTable(name = "user_reel",
+	           joinColumns=@JoinColumn(name="reelId"),
+	           inverseJoinColumns=@JoinColumn(name="userId")) 
+	private List<User> users;
 
 }
--- aaharaSetu/src/main/java/com/spring/aaharaSetu/model/User.java
+++ aaharaSetu/src/main/java/com/spring/aaharaSetu/model/User.java
@@ -1,8 +1,11 @@
 package com.spring.aaharaSetu.model;
 
+import java.util.List;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,5 +24,8 @@ public class User {
 	
 	@OneToOne(mappedBy="user" , cascade = CascadeType.ALL)
 	private UserDetails userDeatils;
+	
+	@ManyToMany(mappedBy = "users" , cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
+	private List<Reel> reels;
 
 }
