6f1ee2990a7444dec95585ac17f64f714d74d959
Chethana <chethanas1302@gmail.com>
2025-07-16 23:57:00 +0530

    Made change in the primary key type(Integer to Long) in the signature of the interface

--- aaharaSetu/src/main/java/com/spring/aaharaSetu/repository/UserRepository.java
+++ aaharaSetu/src/main/java/com/spring/aaharaSetu/repository/UserRepository.java
@@ -2,17 +2,18 @@ package com.spring.aaharaSetu.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.spring.aaharaSetu.model.User;
 
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
 	
 	
-	@Query("SELECT u FROM User u WHERE" +
+	@Query("SELECT u FROM User u WHERE " +
 	       "LOWER(u.userName) LIKE LOWER(CONCAT('%', :keyword , '%'))")
-	User searchUserByName(String keyword);
+	User searchUserByName(@Param("keyword") String keyword);
 
 }
