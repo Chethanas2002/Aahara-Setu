e99bb10297fcc9946721d070e7122174227add53
Chethana <chethanas1302@gmail.com>
2025-07-17 01:53:04 +0530

    feat : added all the search methods for city repo

--- aaharaSetu/src/main/java/com/spring/aaharaSetu/repository/CityRepository.java
+++ aaharaSetu/src/main/java/com/spring/aaharaSetu/repository/CityRepository.java
@@ -1,12 +1,40 @@
 package com.spring.aaharaSetu.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.spring.aaharaSetu.model.City;
 
 
 @Repository
-public interface CityRepository extends JpaRepository<City, Integer>{
+public interface CityRepository extends JpaRepository<City, Long>{
 
+//	Get city by name
+	City findByCityName(String name);
+	
+//	Get city by name partial match and case insensitive 
+	List<City> findByCityNameContainingIgnoreCase(String name);
+	
+//	Gets all the city ordered in ascending order of city name
+	List<City> findAllByOrderByCityNameAsc();
+	
+//	Get cities based on specific state exact match and case insensitive
+//	List<City> findByState_StateNameIgnoreCase(String name);
+	@Query("SELECT c FROM City c "+
+	       "JOIN c.state s "+
+		   "WHERE LOWER(s.stateName) = LOWER(:stateName)")
+	List<City> searchCitiesByState(@Param("stateName") String stateName);
+	
+	
+//	Get cities based on specific state exact match and case insensitive
+//	List<City> findByState_StateNameContainingIgnoreCase(String name);
+	@Query("SELECT c FROM City c "+
+			"JOIN c.state s "+
+			"WHERE LOWER(s.stateName) LIKE LOWER(CONCAT('%',:stateName,'%'))")
+	List<City> searchCitiesByStatePartialSearch(@Param("stateName") String stateName);
 }
+
